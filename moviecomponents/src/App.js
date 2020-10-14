4b839dd7a4a2df1ced7e5bad4aa8582c57306966
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-10-14 01:14:53 -0500

    added comments to make code more readable

--- moviecomponents/src/App.js
+++ moviecomponents/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Search from "./components/MovieList";
 
 function App() {
   return (
     <div className="App">
       <h1>Movie API</h1>
-      <MovieList />
+      <Search />
 
       
     </div>
